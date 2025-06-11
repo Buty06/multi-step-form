a850ec4d053ad1bcea22278b73a490803dd28f89
Buty06 <mecagoenlashormigas@gmail.com>
2025-06-10 21:00:59 -0400

    Agregando el useRef para controlar el formulario, pero esta en desarrollo

--- src/Components/Home/HomeMain.tsx
+++ src/Components/Home/HomeMain.tsx
@@ -1,14 +1,21 @@
+import { useRef } from "react";
 import { HomeMainDescription } from "./HomeMainDescription";
 import { HomeMainForm } from "./HomeMainForm";
 
 export const HomeMain = () => {
+  const formRef = useRef<HTMLFormElement>(null);
+
+  if (formRef.current) {
+    console.log(formRef.current);
+  }
+
   return (
     <main>
       {/* Description */}
       <HomeMainDescription></HomeMainDescription>
 
       {/* Form */}
-      <HomeMainForm></HomeMainForm>
+      <HomeMainForm ref={formRef}> </HomeMainForm>
     </main>
   );
 };
