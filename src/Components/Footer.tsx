18abd40819fa73ff9a848a10b2d1b42b242663f0
Buty06 <mecagoenlashormigas@gmail.com>
2025-06-19 13:58:38 -0400

    Habilitando los botones nex y back en el footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,23 +1,34 @@
+import { useLocation, useNavigate } from "react-router-dom";
 import "../Styles/Home/Footer.css";
 
-interface Props {
-  backButtonIsNotRequired?: boolean;
-  handlerExternalSubmit?: (event: React.FormEvent) => void;
-}
+const steps = ["/", "/plans", "/addons", "/finishing"];
 
-export const Footer: React.FC<Props> = ({
-  backButtonIsNotRequired,
-  handlerExternalSubmit,
-}) => {
+export const Footer: React.FC = () => {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+  const currentIndex = steps.indexOf(pathname);
+
+  const nextStep = () => {
+    currentIndex < steps.length - 1 && navigate(steps[currentIndex + 1]);
+  };
+
+  const backStep = () => {
+    navigate(steps[currentIndex - 1]);
+  };
+
   return (
     <footer className="footer">
       {/* Next Button */}
 
-      <button onClick={handlerExternalSubmit} className="footer_next_button">
+      <button onClick={nextStep} className="footer_next_button">
         Next Step
       </button>
 
-      {!backButtonIsNotRequired && <button>Go Back</button>}
+      {!(pathname === "/") && (
+        <button onClick={backStep} className="footer_back_button">
+          Go Back
+        </button>
+      )}
     </footer>
   );
 };
