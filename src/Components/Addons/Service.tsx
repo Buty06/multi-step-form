417c2275da950d866588259d971d9cbf569db210
Buty06 <acnielwilliams08@gmail.com>
2025-07-07 18:17:40 -0400

    Terminando el componente service con la clase active_checkbox

--- src/Components/Addons/Service.tsx
+++ src/Components/Addons/Service.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "../../Styles/Addons/Service.css";
 
 interface props {
@@ -7,9 +8,28 @@ interface props {
 }
 
 export const Service: React.FC<props> = ({ title, description, price }) => {
+  const [isChecked, setIsChecked] = useState(Boolean);
+
+  const checked = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.checked;
+
+    setIsChecked(value);
+  };
+
+  const active_checkbox = () => {
+    if (!isChecked) return "service";
+
+    return "service active_checkbox";
+  };
+
   return (
-    <article className="service">
-      <input type="checkbox" name="checkbox" className="checkbox" />
+    <article className={active_checkbox()}>
+      <input
+        type="checkbox"
+        name="checkbox"
+        className="checkbox"
+        onChange={(e) => checked(e)}
+      />
 
       <section className="text_container">
         <h3 className="title"> {title} </h3>
