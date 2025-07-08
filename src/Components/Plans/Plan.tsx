8fbd5ca5683c05a060cd9c09d33703cc2e1f9e83
Buty06 <acnielwilliams08@gmail.com>
2025-07-08 10:46:45 -0400

    Intentando consumir el objeto global de review

--- src/Components/Plans/Plan.tsx
+++ src/Components/Plans/Plan.tsx
@@ -1,3 +1,6 @@
+import { useContext } from "react";
+import { ReviewContext } from "../../Context/ReviewContext";
+
 // Definimos los tipados de las props
 interface Props {
   title: string;
@@ -6,8 +9,23 @@ interface Props {
 }
 
 export const Plan: React.FC<Props> = ({ title, price, src }) => {
+  const { setReview, review } = useContext(ReviewContext)! as {
+    setReview: object;
+  };
+
+  const reviewPlan = {
+    title,
+    price,
+  };
+
+  const sendPlan = () => {
+    setReview(reviewPlan);
+  };
+
+  console.log(review);
+
   return (
-    <article className="plan">
+    <article className="plan" onClick={sendPlan}>
       <img src={src} alt="Icon" />
 
       <section className="plan_section">
