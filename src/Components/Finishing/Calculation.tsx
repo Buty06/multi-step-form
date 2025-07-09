6bb2115c5303ab442f364e2e2f420df32820e071
Buty06 <acnielwilliams08@gmail.com>
2025-07-09 11:06:04 -0400

    Terminando de comprobar el componente Calculate.tsx, ahora solo queda renderizarlo

--- src/Components/Finishing/Calculation.tsx
+++ src/Components/Finishing/Calculation.tsx
@@ -1,11 +1,14 @@
 import { useContext } from "react";
 import { ReviewContext } from "../../Context/ReviewContext";
+import { TextContext } from "../../Context/TextContext";
 
 export const Calculation = () => {
   const { review } = useContext(ReviewContext)! as {
     // setReview : (value:object) => void
     review: object;
   };
+
+  const { jsonDecision } = useContext(TextContext)!;
 
   let total = 0;
 
@@ -20,5 +23,7 @@ export const Calculation = () => {
     total += number;
   });
 
-  console.log(total);
+  jsonDecision === "month"
+    ? console.log(`+$${total}/mo`)
+    : console.log(`$${total}/yr`);
 };
